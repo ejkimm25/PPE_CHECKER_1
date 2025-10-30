33a6d203d30f3b547a19683de1dfb394bacae7a7
ejkimm25 <eunjeongkim93@gmail.com>
2025-10-30 09:45:03 +0900

    Give execute permission to startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,2 @@
-#!/bin/bash
-
-# 1. 함수 실행
-cd PPE_Function
-python3.11 -m venv .venv
-source .venv/bin/activate
-func start &
-
-# 2. Streamlit 실행
-cd ..
-python3.11 -m venv .venv
-source .venv/bin/activated
-streamlit run app.py --server.port=8000 --server.address=0.0.0.0
+pip install streamlit
+python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0
